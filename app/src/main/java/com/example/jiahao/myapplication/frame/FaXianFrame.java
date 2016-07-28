80cef6177274cadac0ea7bb7b8781a6e72a5b80d
okbexx <857085930@qq.com>
2016-07-28 23:17:30 +0800

    sdf

--- app/src/main/java/com/example/jiahao/myapplication/frame/FaXianFrame.java
+++ app/src/main/java/com/example/jiahao/myapplication/frame/FaXianFrame.java
@@ -1,154 +1,20 @@
 package com.example.jiahao.myapplication.frame;
 
-import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 
 import com.example.jiahao.myapplication.R;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Jiahao on 2016/7/22.
  */
-public class FaXianFrame extends Fragment {
+public class FaXianFrame extends android.support.v4.app.Fragment {
     @Nullable
-
-    private FragmentManager fm;
-    private static int startID = 0;
-
-    List<Class> list = new ArrayList<Class>();
-
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.frame_faxian, container, false);
-
-        init();
-        final RadioGroup radioGroup = (RadioGroup) view.findViewById(R.id.rg_fenlei_menue);
-        final RadioButton rb = (RadioButton) radioGroup.getChildAt(0);
-        rb.setTextColor(radioGroup.getResources().getColor(R.color.color_bt_checked));
-
-        final ViewPager vp = (ViewPager) view.findViewById(R.id.vp_faxian);
-
-
-        vp.setAdapter(new MyvpAdapter(getActivity().getSupportFragmentManager()));
-        vp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                switch (position) {
-                    case 0:
-                        ((RadioButton) radioGroup.getChildAt(0)).setChecked(true);
-                        break;
-                    case 1:
-                        ((RadioButton) radioGroup.getChildAt(1)).setChecked(true);
-                        break;
-                    case 2:
-                        ((RadioButton) radioGroup.getChildAt(2)).setChecked(true);
-                        break;
-                    case 3:
-                        ((RadioButton) radioGroup.getChildAt(3)).setChecked(true);
-                        break;
-                    case 4:
-                        ((RadioButton) radioGroup.getChildAt(4)).setChecked(true);
-                        break;
-                    case 5:
-                        ((RadioButton) radioGroup.getChildAt(5)).setChecked(true);
-                        break;
-
-                }
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-        startID = rb.getId();
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                rbChecked(group, checkedId);
-                if(startID == checkedId){
-                    startID = group.getChildAt(0).getId();
-                }
-
-                switch (checkedId) {
-                    case R.id.rb_faxian_jinxuan:
-                        vp.setCurrentItem(0);
-                        break;
-
-                    case R.id.rb_faxian_chuanda:
-                        vp.setCurrentItem(1);
-                        break;
-
-                    default:
-                        vp.setCurrentItem(2);
-                }
-                startID = checkedId;
-            }
-        });
-
-        return view;
-    }
-
-    private void rbChecked(RadioGroup group, int checkedId) {
-        RadioButton rb = (RadioButton) group.findViewById(startID);
-        rb.setTextColor(group.getResources().getColor(R.color.color_text_default));
-        RadioButton rb2 = (RadioButton) group.findViewById(checkedId);
-        rb2.setTextColor(group.getResources().getColor(R.color.color_bt_checked));
-    }
-
-    private void init() {
-        list.add(FaxianFrame_item1.class);
-        list.add(FaxianFrame_item2.class);
-        list.add(FaxianFrame_item3.class);
-        list.add(FaxianFrame_item4.class);
-        list.add(FaxianFrame_item5.class);
-        list.add(FaxianFrame_item6.class);
-
-    }
-
-    private class MyvpAdapter extends FragmentPagerAdapter {
-
-        public MyvpAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            try {
-                return (Fragment) list.get(position).newInstance();
-            } catch (java.lang.InstantiationException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-
-        @Override
-        public int getCount() {
-            return list.size();
-        }
+        return inflater.inflate(R.layout.frame_faxian, null);
     }
 }
--- app/src/main/java/com/example/jiahao/myapplication/frame/GouWuCheFrame.java
+++ app/src/main/java/com/example/jiahao/myapplication/frame/GouWuCheFrame.java
@@ -5,6 +5,10 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.jiahao.myapplication.R;
 
@@ -12,9 +16,76 @@ import com.example.jiahao.myapplication.R;
  * Created by Jiahao on 2016/7/22.
  */
 public class GouWuCheFrame extends android.support.v4.app.Fragment {
+
+    private View view;
+    private int[] imageId;
+    private String[] text;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frame_gouwuche, null);
+        view = inflater.inflate(R.layout.frame_gouwuche, null);
+        initCode();
+        initUI();
+        return view;
+
+    }
+
+    private void initCode() {
+        imageId = new int[]{R.drawable.daluyouqi,R.drawable.shui,R.drawable.huayin,R.drawable.fengshan,
+                R.drawable.daluyouqi,
+                R.drawable.daluyouqi,R.drawable.daluyouqi,R.drawable.daluyouqi,
+                R.drawable.daluyouqi,R.drawable.daluyouqi,R.drawable.daluyouqi};
+        text = new String[]{"[京东超市]话音水淹素颜","[京东超市]话音水淹素颜","[京东超市]话音水淹素颜","[京东超市]话音水淹素颜","[京东超市]话音水淹素颜","[京东超市]话音水淹素颜","[京东超市]话音水淹素颜","[京东超市]话音水淹素颜","[京东超市]话音水淹素颜","[京东超市]话音水淹素颜","[京东超市]话音水淹素颜"};
+    }
+
+    private void initUI() {
+        GridView gv_tuiJianShangPin = (GridView) view.findViewById(R.id.gv_tuijianshangpin);
+        gv_tuiJianShangPin.setAdapter(new MyAdapter());
+    }
+
+    private class MyAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return imageId.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return imageId[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder viewHolder=null;
+            if(convertView==null) {
+                viewHolder=new ViewHolder();
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_tuijianshangpin, null);
+                viewHolder.imageView= (ImageView) convertView.findViewById(R.id.iv_shangpintu);
+                viewHolder.tv_JieShao= (TextView) convertView.findViewById(R.id.tv_jieshao);
+                viewHolder.tv_Price= (TextView) convertView.findViewById(R.id.tv_price);
+                viewHolder.iv_GouWuChe= (ImageView) convertView.findViewById(R.id.iv_tubiao_gouwuche);
+                convertView.setTag(viewHolder);
+            }
+            else{ viewHolder= (ViewHolder) convertView.getTag();}
+            viewHolder.imageView.setImageResource(imageId[position]);
+            viewHolder.tv_JieShao.setText(text[position]);
+            viewHolder.tv_Price.setText("500");
+            viewHolder.iv_GouWuChe.setImageResource(R.drawable.tupian_gouwuche);
+            return convertView;
+        }
+
+        private class ViewHolder {
+            ImageView imageView;
+            TextView tv_JieShao;
+            TextView tv_Price;
+            ImageView iv_GouWuChe;
+
+        }
     }
 }
